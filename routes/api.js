e7304b72011aac1bf5e617190bfae1b42a5fa0a4
Dan Kwon <dankwon@Dans-Air-2.home>
2015-08-03 02:08:53 -0400

    updated api routes with new controllers

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var Profile = require('../models/Profile');
+var NFLPlayer = require('../models/NFLPlayer');
 var profileController = require('../controllers/ProfileController.js');
 var groupController = require('../controllers/GroupController.js');
-var controllers = {'profile':profileController, 'group':groupController};
+var rosterController = require('../controllers/RosterController.js');
+var nflPlayerController = require('../controllers/NFLPlayerController.js');
+var controllers = {'profile':profileController, 'group':groupController, 'roster':rosterController, 'nflplayer':nflPlayerController};
 var twilio = require('twilio');
+var fs = require('fs');
 
 
 /* GET home page. */
 router.get('/:resource', function(req, res, next) {
+	
+	var resource = req.params.resource;
+	if (resource == 'nflroster'){
+		fs.readFile('public/resources/nflplayers.json', 'utf8', function (err, data) {
+		  if (err) {
+	  		res.send({'confirmation':'fail', 'message':err.message});
+			return;
+		  }
+		  
+		  res.setHeader('content-type', 'application/json');
+		  var playersJson = JSON.parse(data); // this is an array
+		  
+		  var players = new Array();
+		  for (var i=0; i<playersJson.length; i++){
+			  var player = playersJson[i];
+			  
+			  var nflPlayer = new NFLPlayer();
+			  nflPlayer['fullName'] = player.Name;
+			  
+			  var parts = player.Name.split(' ');
+			  nflPlayer['firstName'] = parts[0];
+			  nflPlayer['lastName'] = parts[parts.length-1];
+			  
+			  nflPlayer['fantasyPlayerKey'] = player.FantasyPlayerKey;
+			  nflPlayer['position'] = player.Position;
+			  nflPlayer['team'] = player.Team;
+			  nflPlayer['value'] = 1000 * player.ProjectedFantasyPoints;
+			  nflPlayer.save();
+			  players.push(nflPlayer.summary());
+		  }
+		  
+		  
+//		  var json = JSON.stringify(playersJson, null, 2);
+		  var json = JSON.stringify(players, null, 2); // this makes the json 'pretty' by indenting it
+		  res.send(json);
+		});
+		
+		
+		return;
+	}
 	
 	var controller = controllers[req.params.resource];
 	if (controller == null){
