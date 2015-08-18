66f4317291e3b6dd7902213e1a365224db47449a
Dan Kwon <dankwon@Dans-Air-2.home>
2015-08-17 23:30:27 -0400

    updated contest model

--- models/Contest.js
+++ models/Contest.js
@@ -3,18 +3,23 @@ var mongoose = require('mongoose');
 var ContestSchema = new mongoose.Schema({
 	creator: {type:String, default:''}, // creator of the contest
 	group: {type:String, default:''},
+	season: {type:String, trim:true, default:'2015REG'}, 
+	week: {type:String, trim:true, default:'1'},
 	title: {type:String, trim:true, default:''},
 	state: {type:String, trim:true, default:'open'}, // open, pending, or closed. peanding means games have started but not all are finished.
-	categories: {type:Array, default:[]}, // array of statistical categories to use for scoring
 	entries: {type:Array, default:[]},
+	participants: {type:Array, default:[]},
 	minEntries: {type:Number, default:10}, // for now, all contests must have a minimum of 10 entries to activate
 	buyIn: {type:Number, default:5},
-	payouts: {type:Array, default:[]},
+	payouts: {type:Array, default:[9]},
+	results: {type:mongoose.Schema.Types.Mixed, default:{}},
+	ranking: {type:Array, default:[]}, // { profileId:score }
+	activated: {type:Date, default:Date.now},
 	timestamp: {type:Date, default:Date.now},
 });
 
 ContestSchema.methods.summary = function() {
-	return {'creator':this.creator,'group':this.group, 'title':this.title, 'payouts':this.payouts, 'buyIn':this.buyIn, 'state':this.state, 'categories':this.categories, 'entries':this.entries, 'minEntries':this.minEntries, 'timestamp':this.timestamp, 'id':this._id};
+	return {'results':this.results, 'participants':this.participants, 'activated':this.activated, 'week':this.week, 'season':this.season, 'ranking':this.ranking, 'creator':this.creator,'group':this.group, 'title':this.title, 'payouts':this.payouts, 'buyIn':this.buyIn, 'state':this.state, 'entries':this.entries, 'timestamp':this.timestamp, 'id':this._id};
 };
 
 module.exports = mongoose.model('ContestSchema', ContestSchema);
