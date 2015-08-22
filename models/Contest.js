d266ce6a1527f20c0b60c0605a8500c0ba926302
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-08-21 20:15:01 -0400

    added eligible teams and expires properties on Contest model

--- models/Contest.js
+++ models/Contest.js
@@ -9,17 +9,39 @@ var ContestSchema = new mongoose.Schema({
 	state: {type:String, trim:true, default:'open'}, // open, pending, or closed. peanding means games have started but not all are finished.
 	entries: {type:Array, default:[]},
 	participants: {type:Array, default:[]},
+	eligibleTeams: {type:Array, default:[]},
 	minEntries: {type:Number, default:10}, // for now, all contests must have a minimum of 10 entries to activate
 	buyIn: {type:Number, default:5},
 	payouts: {type:Array, default:[9]},
 	results: {type:mongoose.Schema.Types.Mixed, default:{}},
 	ranking: {type:Array, default:[]}, // { profileId:score }
 	activated: {type:Date, default:Date.now},
+	expires: {type:Date, default:Date.now},
 	timestamp: {type:Date, default:Date.now},
 });
 
 ContestSchema.methods.summary = function() {
-	return {'results':this.results, 'participants':this.participants, 'activated':this.activated, 'week':this.week, 'season':this.season, 'ranking':this.ranking, 'creator':this.creator,'group':this.group, 'title':this.title, 'payouts':this.payouts, 'buyIn':this.buyIn, 'state':this.state, 'entries':this.entries, 'timestamp':this.timestamp, 'id':this._id};
+	var summary = {
+		'results':this.results,
+		'participants':this.participants,
+		'eligibleTeams':this.eligibleTeams,
+		'week':this.week,
+		'season':this.season,
+		'ranking':this.ranking,
+		'creator':this.creator,
+		'group':this.group, 
+		'title':this.title,
+		'payouts':this.payouts,
+		'buyIn':this.buyIn,
+		'state':this.state,
+		'entries':this.entries,
+		'activated':this.activated,
+		'expires':this.expires,
+		'timestamp':this.timestamp,
+		'id':this._id
+	};
+	
+	return summary;
 };
 
 module.exports = mongoose.model('ContestSchema', ContestSchema);
