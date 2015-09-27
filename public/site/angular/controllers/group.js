ee955a703a2f1f73bcc35349dc4837bfcbc60cf8
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-09-27 19:17:38 -0400

    invite member functionality

--- public/site/angular/controllers/group.js
+++ public/site/angular/controllers/group.js
@@ -6,6 +6,8 @@ app.controller('GroupController', ['$scope', 'accountService', 'generalService',
 	$scope.credentials = {'email':'', 'password':'', 'name':'', 'isPublic':'no'};
 	$scope.loading = false;
 	$scope.group = null;
+	$scope.currentSection = 'matchups';
+	$scope.invitee = {'name':'', 'email':'', 'phone':''};
 
 	
 	$scope.init = function(){
@@ -34,7 +36,44 @@ app.controller('GroupController', ['$scope', 'accountService', 'generalService',
 		});
 	}
 
-	
+	$scope.invite = function(){
+		if ($scope.invitee.name.length == 0){
+			alert('Please enter a name.');
+			return;
+		}
+
+		if ($scope.invitee.email.length == 0){
+			alert('Please enter an email or phone number.');
+			return;
+		}
+
+		var email = $scope.invitee.email;
+		if ($scope.generalService.validateEmail(email) == false){
+			$scope.invitee['phone'] = email; // this is actually a phone number
+			$scope.invitee['email'] = '';
+		}
+
+		var parts = $scope.invitee.name.split(' ');
+		$scope.invitee['firstName'] = parts[0];
+		if (parts.length > 1)
+			$scope.invitee['lastName'] = parts[parts.length-1];
+
+
+		console.log('INVTE: '+JSON.stringify($scope.invitee));
+		var params = {'group':$scope.group.id, 'invited':[$scope.invitee]};
+
+		RestService.put({resource:'invite', id:$scope.group.id}, params, function(response) {
+			console.log(JSON.stringify(response));
+			if (response.confirmation != 'success'){
+				alert(response.message);
+				return;
+			}
+
+			$scope.invitee = {'name':'', 'email':'', 'phone':''};
+			alert('Invitation Sent!');
+		});
+	}
+
 	
 	
 	$scope.login = function(){
