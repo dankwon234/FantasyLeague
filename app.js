0c0c5f024f01dd8eb7a374cb52ab1272fa487d69
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-09-27 16:29:15 -0400

    imported client-sessions

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var sessions = require('client-sessions');
+
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
+var site = require('./routes/site');
 
 
 // Here we find an appropriate database to connect to, defaulting to localhost if we don't find one.  
@@ -28,10 +31,11 @@ app.locals.delimiters = '<% %>';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// uncomment after placing your favicon in /public app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
+app.use('/site', site);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
