ccf0b0729e2bcfff3e5cc06ea26992f032752aae
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-09-27 18:03:56 -0400

    create new group

--- public/site/angular/controllers/account.js
+++ public/site/angular/controllers/account.js
@@ -3,9 +3,9 @@ var app = angular.module('AccountModule', []);
 app.controller('AccountController', ['$scope', 'accountService', 'generalService', 'uploadService', 'RestService', function($scope, accountService, generalService, uploadService, RestService) {
 	$scope['generalService'] = generalService;
 	$scope.profile = null;
-	$scope.credentials = {'email':'', 'password':'', 'name':''};
+	$scope.credentials = {'email':'', 'password':'', 'name':'', 'isPublic':'no'};
 	$scope.loading = false;
-	$scope.currentSection = 'account';
+	$scope.group = {'title':'', 'password':''};
 
 	
 	$scope.init = function(){
@@ -17,32 +17,35 @@ app.controller('AccountController', ['$scope', 'accountService', 'generalService
 	}
 
 	
-	$scope.viewAccount = function(){
-		if ($scope.profile.id == null)
-			return;
-		
-		window.location.href = '/site/account';
-	}
 	
-	$scope.register = function(){
-		var parts = $scope.credentials.name.split(' ');
-		$scope.credentials['firstName'] = parts[0];
-		if (parts.length > 1)
-			$scope.credentials['lastName'] = parts[parts.length-1];
+	$scope.createGroup = function(){
+		if ($scope.group.title.length == 0){
+			alert('Please Enter a Group Title');
+			return;
+		}
 
-		$scope.loading = true;
-		accountService.register($scope.credentials, function(response, error){
-			if (error != null){
-				$scope.loading = false;
-                alert(error.message);
+		if ($scope.group.password.length == 0){
+			alert('Please Enter a Group Password');
+			return;
+		}
+
+		$scope.group['admin'] = $scope.profile.id;
+		var fullName = $scope.profile.firstName+' '+$scope.profile.lastName;
+		$scope.group['profiles'] = [{'id':$scope.profile.id, 'firstName':$scope.profile.firstName, 'lastName':$scope.profile.lastName, 'fullName':fullName}];
+
+		console.log('CREATE GROUP: '+JSON.stringify($scope.group));
+		RestService.post({resource:'group', id:null}, $scope.group, function(response) {
+			console.log(JSON.stringify(response));
+			if (response.confirmation != 'success'){
+				alert(response.message);
 				return;
 			}
 
-			console.log(JSON.stringify(response));
-			
-//			window.location.href = '/site/account';
+			window.location.href = '/site/group/'+response.group.id;
 		});
 	}
+
+
 	
 	$scope.login = function(){
 		$scope.loading = true;
@@ -53,39 +56,10 @@ app.controller('AccountController', ['$scope', 'accountService', 'generalService
 				return;
 			}
 			
-			window.location.href = '/site/forum';
+			window.location.href = '/site/account';
 		});
 	}
 	
-	$scope.selectWorkshop = function(index){
-		$scope.selectedWorkshop = $scope.workshops[index];
-	}
-	
-	$scope.sendInfoRequest = function(){
-		if ($scope.credentials.email.length == 0){
-			alert('Please Enter Your Email');
-			return;
-		}
-
-		if ($scope.credentials.email.name == 0){
-			alert('Please Enter Your Name');
-			return;
-		}
-		
-		
-		var body = {'email':$scope.credentials.email, 'password':$scope.credentials.password, 'name':$scope.credentials.name};
-		if ($scope.selectedWorkshop != null)
-			body['workshop'] = $scope.selectedWorkshop;
-		
-		
-		RestService.post({resource:'info', id:null}, body, function(response) {
-			if (response.confirmation != 'success')
-				return;
-			
-			alert(response.message);
-			window.location.href = '/site/events';
-		});
-	}
 	
 	
 	
